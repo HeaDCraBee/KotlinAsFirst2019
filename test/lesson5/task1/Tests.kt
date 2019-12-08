f700866610efbd7659b878d375dd01bdbaeb5412
HeaDCraBee <theheadcrabz@gmail.com>
2019-12-08 18:39:51 +0300

    lesson 6 v3, lesson 5, lesson 4

--- test/lesson5/task1/Tests.kt
+++ test/lesson5/task1/Tests.kt
@@ -236,11 +236,9 @@ class Tests {
     @Test
     @Tag("Normal")
     fun canBuildFrom() {
-        /*assertFalse(canBuildFrom(emptyList(), "foo"))
+        assertFalse(canBuildFrom(emptyList(), "foo"))
         assertTrue(canBuildFrom(listOf('a', 'b', 'o'), "baobab"))
-        assertFalse(canBuildFrom(listOf('a', 'm', 'r'), "Marat"))*/
-        assertTrue(canBuildFrom(listOf('a', 'b', 'O'), "Baobab"))
-
+        assertFalse(canBuildFrom(listOf('a', 'm', 'r'), "Marat"))
     }
 
     @Test
